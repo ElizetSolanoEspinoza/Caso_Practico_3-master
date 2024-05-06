23b287d6b38589b3a5636b3d080e6fd9cfb101e4
ElizetSolanoEspinoza <167382251+ElizetSolanoEspinoza@users.noreply.github.com>
2024-05-06 12:40:28 -0600

    Add files via upload

---
+++ encabezado.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+<center><img width="750" height="220" src="nueva-imagen.jpg"/>
+<h3 id="centrado">ENCUESTA DE INSEGURIDAD DEL CIUDADANO
+EN LA CIUDAD DE LIMA
+</h3>
+</body>
+</html>
---
+++ pie.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+<h4 id="centrado">Todos los derechos reservados
+Elizet Marintia Solano Espinoza</h4>
+<center><img width="120" height="120" src="SISTEMAS.gif"/>
+</body>
+</html>
